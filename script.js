6697e8c2c475d3ebe2ceb26f34cd7961c3245848
Hutts96 <84289781+Hutts96@users.noreply.github.com>
2023-09-11 22:06:26 +0300

    Add hover effect

--- script.js
+++ script.js
@@ -7,23 +7,12 @@ let divs = [...Array(16)].map(e => Array(16));
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 17; j++) {
         divs[i][j] = document.createElement('div');
-        //divs[i][j].style.border = '2px solid black';
-        //divs[i][j].style.margin = '5px';
-        //divs[i][j].style.flex = '1';
     }
 }
-/*for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        divs[i][j].style.border = '2px black solid';
-        divs[i][j].style.height = '10px';
-    }
-}*/
-/*container.style.display = 'flex';
-container.classList.add('container');*/
+
 for (let i = 0; i < 16; i++) {
     divs[i][0].classList.add('row');
     divs[i][0].style.display = 'flex';
-    //divs[i][0].style.padding = '5px';
     divs[i][0].style.flex = '1';
     divs[i][0].style.justifyContent = 'center';
     container.appendChild(divs[i][0]);
@@ -32,7 +21,21 @@ for (let i = 0; i < 16; i++) {
         divs[i][0].appendChild(divs[i][j]);
         divs[i][j].style.height = '5vmin';
         divs[i][j].style.border = '1px solid black';
-        divs[i][j].style.width = '5vmin'
+        divs[i][j].style.width = '5vmin';
+        divs[i][j].style.backgroundColor = 'white';
+
+    }
+}
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 1; j < 17; j++) {
+        divs[i][j].addEventListener('mouseover', () => {
+            divs[i][j].style.backgroundColor = 'black';
+        })
+        divs[i][j].addEventListener('mouseout', () => {
+            setTimeout(() => { divs[i][j].style.backgroundColor = 'white'; }, 500);
+
+        })
     }
 }
 
