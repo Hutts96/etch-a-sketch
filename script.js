010b2539c26461fc29e6a85ee671ffe3a61ce7d2
Hutts96 <84289781+Hutts96@users.noreply.github.com>
2023-09-12 08:53:45 +0300

    Add button to set size of grid

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-const body = document.querySelector('body');
-body.style.cssText = 'padding: 10px;';
+/*
+TODO:
+    add way to reset dom before creating new grid
+
+*/
+function createGrid(height = 16, width = 16) {
+    divs = [...Array(height)].map(e => Array(width + 1));
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width + 1; j++) {
+            divs[i][j] = document.createElement('div');
+        }
+    }
+
+    for (let i = 0; i < height; i++) {
+        divs[i][0].classList.add('row');
+        divs[i][0].style.display = 'flex';
+        divs[i][0].style.flex = '1';
+        divs[i][0].style.justifyContent = 'center';
+        container.appendChild(divs[i][0]);
+        for (let j = 1; j < width + 1; j++) {
+            divs[i][j].classList.add('column');
+            divs[i][0].appendChild(divs[i][j]);
+            divs[i][j].style.height = '5vmin';
+            divs[i][j].style.border = '1px solid black';
+            divs[i][j].style.width = '5vmin';
+            divs[i][j].style.backgroundColor = 'white';
+
+        }
+    }
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 1; j < width + 1; j++) {
+            divs[i][j].addEventListener('mouseover', () => {
+                divs[i][j].style.backgroundColor = 'black';
+            })
+
+        }
+    }
+}
+function reset() {
+    container.innerHTML = '';
+}
+let body = document.querySelector('body');
 let container = document.createElement('div');
-container.style.cssText = 'display: flex; flex-direction: column; flex: 1;'
-body.appendChild(container);
 let divs = [...Array(16)].map(e => Array(16));
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 17; j++) {
-        divs[i][j] = document.createElement('div');
-    }
-}
+const button = document.querySelector('button');
+body.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
-    divs[i][0].classList.add('row');
-    divs[i][0].style.display = 'flex';
-    divs[i][0].style.flex = '1';
-    divs[i][0].style.justifyContent = 'center';
-    container.appendChild(divs[i][0]);
-    for (let j = 1; j < 17; j++) {
-        divs[i][j].classList.add('column');
-        divs[i][0].appendChild(divs[i][j]);
-        divs[i][j].style.height = '5vmin';
-        divs[i][j].style.border = '1px solid black';
-        divs[i][j].style.width = '5vmin';
-        divs[i][j].style.backgroundColor = 'white';
+body.style.cssText = 'padding: 10px;';
+container.style.cssText = 'display: flex; flex-direction: column; flex: 1;';
 
-    }
-}
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 1; j < 17; j++) {
-        divs[i][j].addEventListener('mouseover', () => {
-            divs[i][j].style.backgroundColor = 'black';
-        })
-        divs[i][j].addEventListener('mouseout', () => {
-            setTimeout(() => { divs[i][j].style.backgroundColor = 'white'; }, 500);
+createGrid();
 
-        })
-    }
-}
+button.addEventListener('click', () => {
+    let height;
+    let width;
+    do {
+        height = Number(prompt('Height (max:100):'));
+    } while (height < 1 || height > 100)
+    do {
+        width = Number(prompt('Width (max:100):'));
+    } while (width < 1 || width > 100)
+    reset();
+    createGrid(height, width, true);
+})
+
 
 
 
